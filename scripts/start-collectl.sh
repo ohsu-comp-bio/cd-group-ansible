b1b7c65b61d6a4fb1fa0dcfe26c381e22877af43
Brian King <kibri@ohsu.edu>
2017-03-07 18:41:35 +0000

    restore dcc-spark-worker-6

--- scripts/start-collectl.sh
+++ scripts/start-collectl.sh
@@ -4,10 +4,6 @@
 CMD="sudo /etc/init.d/collectl start"
 echo collect stats on workers.
 for i in {1..12}; do
-  if [ $i -eq  6 ]
-  then
-	  continue
-  fi
   echo start collectl on dcc-spark-worker-${i}
   ssh dcc-spark-worker-${i} "$CMD"
 done
--- scripts/stop-collectl.sh
+++ scripts/stop-collectl.sh
@@ -3,10 +3,6 @@
 CMD="sudo /etc/init.d/collectl stop"
 echo stop collecting stats on workers.
 for i in {1..12}; do
-  if [ $i -eq 6 ]
-  then
-	  continue
-  fi
   echo stop collectl on dcc-spark-worker-${i}
   ssh dcc-spark-worker-${i} "$CMD"
 done
